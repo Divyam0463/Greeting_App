049175106803218dc2ef47531396bacff150853f
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-04-22 23:18:06 +0530

    Implemented UC2

---
+++ src/main/java/com/example/greeting/Service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.example.greeting.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+  public String getService(){
+    return "hello world" ; 
+  }
+}
