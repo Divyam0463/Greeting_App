139c86b597784e1fda39d71ce51e1db536da33bf
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-04-23 00:04:51 +0530

    Implemented UC4

--- src/main/java/com/example/greeting/Model/Greeting.java
+++ src/main/java/com/example/greeting/Model/Greeting.java
@@ -1,7 +1,21 @@
 package com.example.greeting.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Greeting {
+
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  @Id
+    private Long id ;
+
+    @Column
     private String message;
+    @Column
     private String sender;
 
     public String getMessage() {
@@ -18,6 +32,14 @@ public class Greeting {
 
     public void setSender(String sender) {
       this.sender = sender;
+    }
+
+    public Long getId() {
+      return id;
+    }
+
+    public void setId(Long id) {
+      this.id = id;
     }
 }
 
